dce441d7af35bc999affc5904381d2d4d7bd6018
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2023-04-11 12:53:08 -0400

    Fixed logout feature, added delete account support

---
+++ src/js/deleteaccount.js
@@ -0,0 +1,26 @@
+function deleteAccount () {
+    if (confirm("Delete this account permanently?")) {
+        eraseCookieFromAllPaths("username");
+        eraseCookieFromAllPaths("password");
+        eraseCookieFromAllPaths("lastName");
+        eraseCookieFromAllPaths("firstName");
+        eraseCookieFromAllPaths("numCars");
+        window.location.href = "../../index.html";
+    } else { 
+        console.log("Delete account cancelled");
+    }
+}
+
+function eraseCookieFromAllPaths(name) {
+    // This function will attempt to remove a cookie from all paths.
+    var pathBits = location.pathname.split('/');
+    var pathCurrent = ' path=';
+
+    // do a simple pathless delete first.
+    document.cookie = name + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT;';
+
+    for (var i = 0; i < pathBits.length; i++) {
+        pathCurrent += ((pathCurrent.substr(-1) != '/') ? '/' : '') + pathBits[i];
+        document.cookie = name + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT;' + pathCurrent + ';';
+    }
+}
---
+++ src/js/logout.js
@@ -0,0 +1,8 @@
+function logout () {
+    console.log("Attempting to log out")
+
+    console.log("Logged out")
+    window.location.href = "../../index.html";
+}
+
+
