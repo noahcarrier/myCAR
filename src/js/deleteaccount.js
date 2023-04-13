cf1e86ea05779a4c8d9592c9f21cb761b285d63d
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2023-04-13 17:46:47 -0400

    Added password requirement to delete account

--- src/js/deleteaccount.js
+++ src/js/deleteaccount.js
@@ -1,5 +1,14 @@
 function deleteAccount () {
-    if (confirm("Delete this account permanently?")) {
+    var deleteAccount = false;
+    var verifyPassword = prompt("Confirm your account deletion by typing your password:");
+    var password = getPassword();
+    if (verifyPassword === password) {
+        deleteAccount = confirm("Are you sure you want to delete your account?");
+    }
+    else {
+        alert("Incorrect password");
+    }
+    if (deleteAccount) {
         eraseCookieFromAllPaths("username");
         eraseCookieFromAllPaths("password");
         eraseCookieFromAllPaths("lastName");
@@ -22,5 +31,17 @@ function eraseCookieFromAllPaths(name) {
     for (var i = 0; i < pathBits.length; i++) {
         pathCurrent += ((pathCurrent.substr(-1) != '/') ? '/' : '') + pathBits[i];
         document.cookie = name + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT;' + pathCurrent + ';';
+    }
+}
+
+
+function getPassword () {
+    var credentials = document.cookie.split("; ");
+    for (var i = 0; i < credentials.length; i++){
+        var cookieValueArray = credentials[i].split("=");
+    
+        if (cookieValueArray[0] == "password"){
+            return cookieValueArray[1]
+        }
     }
 }
